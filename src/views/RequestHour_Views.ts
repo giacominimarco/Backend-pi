521b1249fe0c49becc7bdbfdb51688df69317ba0
pedro <phbs1235@gmail.com>
2020-12-14 23:41:49 -0300

    new values

--- src/views/RequestHour_Views.ts
+++ src/views/RequestHour_Views.ts
@@ -3,6 +3,8 @@ import Files_Views from './Files_Views';
 import File from '../models/File';
 
 interface indexRequestHourProps {
+  id: string;
+  states_id: string;
   name: string;
   lastName: string;
   registration: number;
@@ -17,6 +19,8 @@ interface indexRequestHourProps {
 export default {
   render(requestHour: indexRequestHourProps) {
     return {
+      id: requestHour.id,
+      states_id: requestHour.states_id,
       name: requestHour.name,
       lastName: requestHour.lastName,
       registration: requestHour.registration,
